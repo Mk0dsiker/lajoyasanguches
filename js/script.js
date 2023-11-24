a76f1acced9a74c8414cfcaf79257e24207cbfd9
Mk0dsiker <mko.bartender@gmail.com>
2023-11-24 00:02:29 -0300

    Añadiendo menu lateral con funcionalidad on off

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function menuOn() {
+    var menu = document.querySelector(".menu");
+    menu.style.transform = "none";
+    
+    
+    
+}
+
+function menuOff() {
+    var menu = document.querySelector(".menu");
+    menu.style.transform = "translateX(100%)";
+    
+    
+    
+}

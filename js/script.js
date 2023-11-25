ecc92bdaaa7b1bdb36fa63cdd1d1d475680353c1
Mk0dsiker <mko.bartender@gmail.com>
2023-11-25 02:11:44 -0300

    Avanzando con el home de la carta

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function menuOn() {
     var menu = document.querySelector(".menu");
     menu.style.transform = "none";
-    
+    document.querySelector('body').classList.add('menu-on');
     
     
 }
@@ -9,7 +9,7 @@ function menuOn() {
 function menuOff() {
     var menu = document.querySelector(".menu");
     menu.style.transform = "translateX(100%)";
-    
+    document.querySelector('body').classList.remove('menu-on');
     
     
 }

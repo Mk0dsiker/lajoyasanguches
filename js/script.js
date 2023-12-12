2060c6840f82062ded63174f60f626faa2dd25be
Mirko Ponce <mko.bartender@gmail.com>
2023-12-12 01:50:04 -0300

    Probando intercambio de idiomas

--- js/script.js
+++ js/script.js
@@ -21,3 +21,32 @@ function mostrar_pagina(element) {
         console.log("Para compartir fue elegido");
     }
 }
+
+function cambiar_idioma_ingles(){
+    document.querySelector(".categories").innerHTML = `<li><a href="index.html" target="_self">Home</a></li>
+    <li><a href="toshare.html" target="_self">To Share</a></li>
+    <li><a href="burger.html" target="_self">Burgers</a></li>
+    <li><a href="veggie.html" target="_self">Veggie</a></li>
+    <li><a href="sanguches.html" target="_self">Sandwish</a></li>
+    <li><a href="plates.html" target="_self">Plates</a></li>
+    <li><a href="salads.html" target="_self">Salads</a></li>
+    <li><a href="postres.html" target="_self">Desert</a></li>`
+
+    document.querySelector(".en").style = "background-color: black;"
+    document.querySelector(".es").style = "background-color: #ffbe00;"
+    
+}
+
+function cambiar_idioma_español(){
+    document.querySelector(".categories").innerHTML =`<li><a href="index.html" target="_self">Home</a></li>
+    <li><a href="toshare.html" target="_self">Para compartir</a></li>
+    <li><a href="burger.html" target="_self">Hamburguesas</a></li>
+    <li><a href="veggie.html" target="_self">Veggie</a></li>
+    <li><a href="sanguches.html" target="_self">Sanguches</a></li>
+    <li><a href="plates.html" target="_self">Platos</a></li>
+    <li><a href="salads.html" target="_self">Ensaladas</a></li>
+    <li><a href="postres.html" target="_self">Postres</a></li>`
+
+    document.querySelector(".es").style = "background-color: black;"
+    document.querySelector(".en").style = "background-color: #ffbe00;"
+}

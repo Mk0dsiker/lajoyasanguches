771edb6917950d1ba955b3e90227ef79386a6141
Mirko Ponce <mko.bartender@gmail.com>
2024-01-08 17:21:44 -0300

    fixing idioma

--- js/script.js
+++ js/script.js
@@ -43,18 +43,20 @@ function idioma() {
 
 
     let id = check.checked;
+    
     if(id == true){
         // location.href = "en/toshare.html"
         location.href = `en/${nameValue}.html`
     } else {
-        location.href = `../${nameValue}.html`
+        // location.href = `../${nameValue}.html`
+        console.log(nameValue);
     }
 }
 
 var check = document.querySelector(".checkm");
 check.addEventListener('click', tipocarta);
 
-function tipocarta() {
+function tipocarta() { 
     var checkm = document.querySelector(".checkm");
 
     let id = checkm.checked;
@@ -67,28 +69,3 @@ function tipocarta() {
     location.href = `index.html`
     }
 }
-
-// function atragos(){
-//     var menu = document.querySelector(".menu").style.backgroundColor = "green";
-//     var categories = document.querySelector(".categories").innerHTML = (`<li><a href="index.html" target="_self">Inicio</a></li>
-//     <li><a href="cantina.html" target="_self">Cantina Joya</a></li>
-//     <li><a href="#" target="_self">Jarras/Sour</a></li>
-//     <li><a href="#" target="_self">Mojitos/Spritz</a></li>
-//     <li><a href="#" target="_self">Mules/Clasicos</a></li>
-//     <li><a href="#" target="_self">Shops/Cervezas</a></li>
-//     <li><a href="#" target="_self">Destilados</a></li>
-//     <li><a href="#" target="_self">Vinos/Espumosos</a></li>`);
-// }
-
-// function acomida(){
-//     console.log("boton desactivado")
-//     var menu = document.querySelector(".menu").style.backgroundColor = "#feb000";
-//     var categories = document.querySelector(".categories").innerHTML = (`<li><a href="index.html" target="_self">Inicio</a></li>
-//     <li><a href="toshare.html" target="_self">Para compartir</a></li>
-//     <li><a href="burger.html" target="_self">Hamburguesas</a></li>
-//     <li><a href="veggie.html" target="_self">Veggie</a></li>
-//     <li><a href="sanguches.html" target="_self">Sanguches</a></li>
-//     <li><a href="plates.html" target="_self">Platos</a></li>
-//     <li><a href="salads.html" target="_self">Ensaladas</a></li>
-//     <li><a href="postres.html" target="_self">Postres</a></li>`);
-// }

0381b2199c261b9ddcde2f2f353b600e05f34503
Mirko Ponce <mko.bartender@gmail.com>
2024-01-15 14:59:29 -0300

    fixing eng pages

--- js/script.js
+++ js/script.js
@@ -23,7 +23,6 @@ function idioma() {
     let namePage = document.querySelector("title").innerText
     console.log(namePage);
 
-
     if(namePage == "Inicio" || namePage == "Home"){
         nameValue = "index";
     } else if(namePage == "Coctel" || namePage == "Cocktail"){
@@ -31,7 +30,7 @@ function idioma() {
     } else if(namePage == "Para Compartir" || namePage == "To Share"){
         nameValue = "toshare";
     } else if(namePage == "Cantina" || namePage == "Canteen"){
-        nameValue = "cantina";
+        nameValue = "cantina";             
     } else if(namePage == "Hamburguesas" || namePage == "Burgers"){
         nameValue = "burger";
     } else if(namePage == "Veggie"){
@@ -67,4 +66,50 @@ function cambiarmenu() {
     } else {
         location.href = `index.html`
     }
+}
+
+window.onload=option_selected();  
+
+function option_selected() {
+    let number = "";
+    let namePage = document.querySelector("title").innerText
+    console.log(namePage);
+
+    if(namePage == "Inicio" || namePage == "Home"){
+        number = "1";
+    } else if(namePage == "Para Compartir" || namePage == "To Share"){
+        number = "2";            
+    } else if(namePage == "Hamburguesas" || namePage == "Burgers"){
+        number = "3";
+    } else if(namePage == "Veggie"){
+        number = "4";
+    } else if(namePage == "Sanguches" || namePage == "Sandwishes"){
+        number = "5";
+    } else if(namePage == "Platos" || namePage == "Main courses"){
+        number = "6";
+    } else if(namePage == "Ensaladas" || namePage == "Salads"){
+        number = "7";
+    } else if(namePage == "Postres" || namePage == "Desserts"){
+        number = "8";
+    }
+
+    if(namePage == "Coctel" || namePage == "Cocktail"){
+        number = "1";
+    } else if(namePage == "Cantina" || namePage == "Canteen"){
+        number = "2";
+    } else if(namePage == "Jarras y Sours" || namePage == "Jar and Sours"){
+        number = "3";
+    } else if(namePage == "Mojitos, Spritz y Fizz" || namePage == "Mojitos, Spritz and Fizz"){
+        number = "4";
+    } else if(namePage == "Destilados" || namePage == "Destiled"){
+        number = "7";
+    } 
+    console.log(number);
+
+    let li = document.querySelector(`#parent :nth-child(${number})`);
+    console.log(li);
+
+    li.style.cssText = 'background-color: black; border-radius: 5px;';
+
+    // children.childNodes(number).classList.add(".selected");
 }
